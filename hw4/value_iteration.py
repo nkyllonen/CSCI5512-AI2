17710ecf6445f4c07e2e4be21d4d6a44b08d5837
Nikki Kyllonen <kyllo089@umn.edu>
2019-04-12 11:30:39 -0500

    hw4(3): more initial setup

--- hw4/value_iteration.py
+++ hw4/value_iteration.py
@@ -12,7 +12,7 @@ Problem 3: Value Iteration
   Given 4 rows x 3 columns Rewards + gamma = 0.8 
 '''
 
-import random, sys
+import random, sys, copy
 
 class Coord:
   def __init__(self, i, j):
@@ -29,19 +29,62 @@ class UCell:
   '''
   UCell: Utility object
     self.utility  (float) : utility value of this cell
-    self.index    (Coord) : (i, j) indices of this cell
     self.best_act (str)   : direction of best action to take
     self.is_end   (bool)  : T/F is this an end state cell
   '''
-  def __init__(self, u, pos, end):
-    self.utility = u
-    self.index = pos
+  def __init__(self, u = None, end = None):
+    self.utility = u or None
+    self.is_end = end or False
     self.best_act = ""
-    self.is_end = end
 
   def __str__(self):
     if (self.is_end == False):
-      return str(self.index) , ' : {0} - {1}'.format(self.utility, self.best_act)
+      return ' {0} - {1} '.format(self.utility, self.best_act)
     else:
-      return str(self.index) , ' : {0}'.format(self.utility)
+      return ' {0} '.format(self.utility)
 
+'''
+has_converged: determine if difference in given utility
+                tables is within epsilon error
+'''
+def has_converged(u0, u1):
+  epsilon = 0.0001
+
+  for i in range(len(u0)):
+    for j in range(len(u0[i])):
+      if abs(u0[i][j].utility - u1[i][j].utility) > epsilon:
+        return False
+  return True
+
+'''
+value_iteration:
+'''
+def value_iteration(results, u_start):
+  # Utility tables
+  u_old = u_start
+  u_new = copy.deepcopy(u_old)
+
+  # Possible actions
+  actions = {'up': Coord(0, -1),
+              'right': Coord(1, 0),
+              'down': Coord(0, 1),
+              'left': Coord(-1, 0)}
+
+  
+
+
+'''
+========= MAIN =========
+'''
+if __name__ == '__main__':
+  results = [[None, 50, None],
+              [None, 0, -3],
+              [-50, -1, -10],
+              [None, -3, -2]]
+
+  u_start = [[UCell(), UCell(50, True), UCell()],
+              [UCell(), UCell(0), UCell(0)],
+              [UCell(-50, True), UCell(0), UCell(0)],
+              [UCell(), UCell(0), UCell(0)]]
+
+  u_final = value_iteration(results, u_start)
