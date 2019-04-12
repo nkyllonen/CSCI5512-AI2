d4b5d451ccce5faa050a624fb83813e46db51f05
Nikki Kyllonen <kyllo089@umn.edu>
2019-04-12 12:07:17 -0500

    hw4(3): building Bellman Equation summation

--- hw4/value_iteration.py
+++ hw4/value_iteration.py
@@ -12,7 +12,7 @@ Problem 3: Value Iteration
   Given 4 rows x 3 columns Rewards + gamma = 0.8 
 '''
 
-import random, sys, copy
+import random, sys, copy, math
 
 class Coord:
   def __init__(self, i, j):
@@ -57,20 +57,69 @@ def has_converged(u0, u1):
   return True
 
 '''
+turn: return Coord representing Coord c rotated deg degrees
+'''
+def turn(c, deg):
+  rad = math.radians(deg)
+  return Coord(c.x*math.cos(rad) - c.y*math.sin(rad), c.x*math.sin(rad) + c.y*math.cos(rad))
+
+'''
 value_iteration:
 '''
-def value_iteration(results, u_start):
+def value_iteration(results, u_start, gamma):
   # Utility tables
   u_old = u_start
   u_new = copy.deepcopy(u_old)
 
   # Possible actions
-  actions = {'up': Coord(0, -1),
+  actions = {'up': Coord(0, 1),
               'right': Coord(1, 0),
-              'down': Coord(0, 1),
+              'down': Coord(0, -1),
               'left': Coord(-1, 0)}
 
-  
+  # Probabilities
+  P_straight = 0.7
+  P_right = 0.15
+  P_left = 0.15
+
+  # Loop until convergence
+  converged = False
+  while (!converged):
+    for i in range(len(u_old)):
+      for j in range(len(u_old[i])):
+        # Calculate updated values --> u_new
+        # Bellman Equation:
+        #   s' = (i', j') = (i, j) + action
+        #   u_new(i,j) = R(i,j) + gamma * MAX_action(SUM_s'(P(s'|s(i,j), action)*u_old(s')))
+        if (u_old[i][j].value != None and u_old[i][j].is_end == False_:
+          sum_a = 0
+          max_sum = -1000
+
+          for a in actions:
+            # we go where we intend to
+            s_next = Coord(i, j) + actions[a]
+            sum_a = sum_a + (P_straight*u_old[s_next.x][s_next.y])
+            
+            # we go to the right instead
+            s_next = Coord(i,j) + turn(actions[a], 90)
+            sum_a = sum_a + (P_right*u_old[s_next.x][s_next.y])
+            
+            # we go to the left instead
+            s_next = Coord(i,j) + turn(actions[a], -90)
+            sum_a = sum_a + (P_left*u_old[s_next.x][s_next.y])
+
+            # compare to max
+            if (sum_a > max_sum):
+              max_sum = sum_a
+            
+        
+      # END for j
+    # END for i
+    converged = has_converged(u_old, u_new)
+
+    # Place new values into u_old
+    u_old = copy.deepcopy(u_new)
+  # END while
 
 
 '''
@@ -87,4 +136,6 @@ if __name__ == '__main__':
               [UCell(-50, True), UCell(0), UCell(0)],
               [UCell(), UCell(0), UCell(0)]]
 
-  u_final = value_iteration(results, u_start)
+  gamma = 0.8
+
+  u_final = value_iteration(results, u_start, gamma)
