e4903cee0a7108dc19d32ba4e9786224aaab6bd7
Nikki Kyllonen <nikki-kyllonen@comcast.net>
2019-05-02 23:26:15 -0500

    hw5(1): setting up files

---
+++ hw5/README.txt
@@ -0,0 +1,13 @@
+======================================
+CSCI 5512 - AI 2
+HW 5 README
+
+Nikki Kyllonen
+kyllo089
+======================================
+
+## Problem 1
+i) run without arguments -- default to 'inTree.csv'
+    $ decision_tree.py
+ii) run with an argument -- provide csv filename
+    $ decision_tree.py <file_containing_examples.csv>
---
+++ hw5/decision_tree.py
@@ -0,0 +1,38 @@
+'''
+CSCI 5512 - AI 2 - HW 5
+Nikki Kyllonen
+kyllo089
+
+Problem 1: output a decision tree using the heuristic
+            based on entropy (as discussed in class)
+'''
+import sys
+
+def build_ex_dict(infile):
+  examples = dict()
+
+  # parse csv column headers
+  headers = infile.readline().strip('\n').split(',')
+
+  for h in headers:
+    examples[h] = list()
+
+  for line in infile:
+    
+  # END for line
+
+  infile.close()
+  return examples
+
+'''
+========= MAIN =========
+'''
+if __name__ == '__main__':
+  # default input file
+  filename = 'inTree.csv'
+  
+  if (len(sys.argv) == 2):
+    filename = str(sys.argv[1])
+
+  infile = open(filename, 'r')
+  examples = build_ex_dict(infile)
